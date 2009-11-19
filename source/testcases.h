9c5ab34b5d4ab57da6c5ff8b56fe040f0bd092c1
jia.shao.peng <jia.shao.peng@1caf6d06-c8a9-11de-8f3f-2f730f80caef>
2009-11-19 10:28:54 +0000

    Check in code for generating test pages.

---
+++ source/testcases.h
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2009 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Definition TestCase for Client URL Internet Emission Sniffer (Curlies)
+ *
+ * @author Shaopeng Jia (jia.shao.peng@gmail.com)
+ */
+#ifndef TESTCASES_H_
+#define TESTCASES_H_
+
+enum TestType {
+  kHost,
+  kPath,
+  kParameter,
+  kQuery,
+  kFormGet,
+};
+
+enum Encoding {
+  kAscii,
+  kBig5,
+  kEncodingSize,
+};
+
+struct TestCase {
+  int test_id;
+  TestType test_component;
+  Encoding test_encoding;
+  const char* test_string_for_display;
+  const char* test_string;
+  bool should_escape; // only in effect when test_string contains
+                      // a single character
+};
+
+extern TestCase entries[];
+extern int entries_size();
+
+#endif  // TESTCASES_H_
